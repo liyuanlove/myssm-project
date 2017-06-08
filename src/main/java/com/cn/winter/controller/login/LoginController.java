a0888b9e78cd7d89d25d5758d71f93c4fa3cfbb0
WinterChenS <1085143002@qq.com>
2017-06-08 21:42:54 +0800

    06081

--- src/main/java/com/cn/winter/controller/login/LoginController.java
+++ src/main/java/com/cn/winter/controller/login/LoginController.java
@@ -2,6 +2,8 @@ package com.cn.winter.controller.login;
 
 import javax.annotation.Resource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,38 +13,39 @@ import com.cn.winter.pojo.User;
 import com.cn.winter.service.user.UserService;
 
 @Controller("loginController")
-public class LoginController extends BaseController{
+public class LoginController extends BaseController {
 
-	@Resource(name="userService")
-	private UserService userService;
-	
-	@RequestMapping(value={"/","login"})
-	public String toLoginInPage(){
-		return "login/login";
-	}
-	
-	
-	@RequestMapping("/user_login")
-	public ModelAndView toLogin(User user, String remember){
-		
-		
-		
-		//验证用户密码是否正确
-		Integer flag = userService.findUserVerification(user);
-		if(flag == null){
-			ModelAndView mv = new ModelAndView();
-			mv.addObject("data", "用户名不存在或密码错误");
-			mv.setViewName("forward:/login");
-			return mv;
-		}else{	//成功登录(技术员)
-			User u = userService.findUserByPhone(user.getPhone());
-			this.request.getSession().setAttribute("userInfo", u);
-			this.request.getSession().setAttribute("userRole", "technician");
-			ModelAndView mv = new ModelAndView();
-			mv.setViewName("/home_page");
-			return mv;
-		}
-		
-	}
-	
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Resource(name = "userService")
+    private UserService userService;
+
+    @RequestMapping(value = {"/", "login"})
+    public String toLoginInPage() {
+        return "login/login";
+    }
+
+
+    @RequestMapping("/user_login")
+    public ModelAndView toLogin(User user, String remember) {
+
+
+        //验证用户密码是否正确
+        Integer flag = userService.findUserVerification(user);
+        if (flag == null) {
+            ModelAndView mv = new ModelAndView();
+            mv.addObject("data", "用户名不存在或密码错误");
+            mv.setViewName("forward:/login");
+            return mv;
+        } else {    //成功登录(技术员)
+            User u = userService.findUserByPhone(user.getPhone());
+            this.request.getSession().setAttribute("userInfo", u);
+            this.request.getSession().setAttribute("userRole", "technician");
+            ModelAndView mv = new ModelAndView();
+            mv.setViewName("/home_page");
+            return mv;
+        }
+
+    }
+
 }
